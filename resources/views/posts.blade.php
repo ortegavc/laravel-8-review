2025899f04a6acedb33bb00b879d8ce0c4a91ab2
Cristhian Ortega <cristhian.ort3ga@gmail.com>
2023-04-05 00:04:02 -0500

    Route model binding using a slug

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,7 +2,7 @@
     @foreach($posts as $post)
     <article>
         <h1>
-            <a href="/post/{{ $post->id }}">
+            <a href="/post/{{ $post->slug }}">
                 {!! $post->title !!}
             </a>
         </h1>
