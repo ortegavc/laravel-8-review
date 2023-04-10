27203b5ad1ccc326a2fdb0edbf9d9b7cc5a69cd9
Cristhian Ortega <cristhian.ort3ga@gmail.com>
2023-04-10 08:30:42 -0500

    Database seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,52 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+        User::truncate();
+        Category::truncate();
+
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+        ]);
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $family->id,
+            'title' => 'My Family Post',
+            'slug' => 'my-family-post',
+            'excerpt' => '<p>Lorem ipsum dolar sit amet.</p>',
+            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $personal->id,
+            'title' => 'My Personal Post',
+            'slug' => 'my-personal-post',
+            'excerpt' => '<p>Lorem ipsum dolar sit amet.</p>',
+            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'My Work Post',
+            'slug' => 'my-work-post',
+            'excerpt' => '<p>Lorem ipsum dolar sit amet.</p>',
+            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
+        ]);
     }
 }
